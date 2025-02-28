bb6727f2b8bd43e40d03948dc48eb99174ab8b0b
Fraser Provan <fraserprovan2@gmail.com>
2025-02-28 00:34:49 +0000

    starting point

---
+++ src/data/dinosaurs.js
@@ -0,0 +1,17 @@
+const dinosaurs = [
+    {
+      name: "Tyrannosaurus Rex",
+      image: "https://www.nhm.ac.uk/discover/dino-directory/tyrannosaurus-rex",
+      period: "Cretaceous",
+      fullPeriod: "Late Cretaceous",
+      yearsMya: {
+        start: 68,
+        end: 66
+      },
+      difficulty: 1 // 1 easy-3 hard (based on how common this dinosaur is, more people know = easier)
+    },
+    //...
+  ];
+  
+  export default dinosaurs;
+  
