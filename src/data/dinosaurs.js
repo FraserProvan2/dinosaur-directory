0def7a578801bc33d22d24b5814c3d67cae39205
Fraser Provan <fraserprovan2@gmail.com>
2025-02-28 13:14:25 +0000

    late triassic done

--- src/data/dinosaurs.js
+++ src/data/dinosaurs.js
@@ -1,35 +1,185 @@
 const dinosaurs = [
   {
-    name: "Tyrannosaurus Rex",
-    image: "tyrannosaurus-rex.png",
-    period: "Cretaceous",
-    fullPeriod: "Late Cretaceous",
-    yearsMya: { start: 68, end: 66 },
-    difficulty: 1
+    name: "Chindesaurus",
+    image: "chindesaurus.png",
+    period: "Triassic",
+    fullPeriod: "Late Triassic",
+    yearsMya: { start: 227, end: 210 },
+    pronunciation: "chin-dee-sore-us",
+    meaning: "'Chinde lizard'",
+    diet: "carnivorous",
+    length: 4.0, // in meters
+    foundIn: ["USA"]
   },
   {
-    name: "Velociraptor",
-    image: "demo.png",
-    period: "Cretaceous",
-    fullPeriod: "Late Cretaceous",
-    yearsMya: { start: 75, end: 71 },
-    difficulty: 2
+    name: "Coelophysis",
+    image: "coelophysis.png",
+    period: "Triassic",
+    fullPeriod: "Late Triassic",
+    yearsMya: { start: 225, end: 190 },
+    pronunciation: "seel-OH-fie-sis",
+    meaning: "'hollow form'",
+    diet: "carnivorous",
+    length: 2.0, // in meters
+    weight: 27, // in kilograms
+    foundIn: ["South Africa", "USA", "Zimbabwe"]
   },
   {
-    name: "Stegosaurus",
-    image: "demo.png",
-    period: "Jurassic",
-    fullPeriod: "Late Jurassic",
-    yearsMya: { start: 155, end: 150 },
-    difficulty: 2
+    name: "Coloradisaurus",
+    image: "coloradisaurus.png",
+    period: "Triassic",
+    fullPeriod: "Late Triassic",
+    yearsMya: { start: 221, end: 210 },
+    pronunciation: "ko-lo-rahd-i-sore-us",
+    meaning: "'[Los] Colorados lizard'",
+    diet: "omnivorous",
+    length: 4.0, // in meters
+    foundIn: ["Argentina"]
   },
   {
-    name: "Brachiosaurus",
-    image: "demo.png",
-    period: "Jurassic",
-    fullPeriod: "Late Jurassic",
-    yearsMya: { start: 154, end: 150 },
-    difficulty: 3
+    name: "Eoraptor",
+    image: "eoraptor.png",
+    period: "Triassic",
+    fullPeriod: "Late Triassic",
+    yearsMya: { start: 231, end: 228 },
+    pronunciation: "EE-oh-rap-tor",
+    meaning: "'dawn thief'",
+    diet: "omnivorous",
+    length: 1.0, // in meters
+    foundIn: ["Argentina"]
+  },
+  {
+    name: "Guaibasaurus",
+    image: "guaibasaurus.png",
+    period: "Triassic",
+    fullPeriod: "Late Triassic",
+    yearsMya: { start: 225, end: 220 },
+    pronunciation: "gwhy-bah-sore-us",
+    meaning: "'Guaíba lizard'",
+    diet: "omnivorous",
+    length: 2.0, // in meters
+    foundIn: ["Brazil"]
+  },
+  {
+    name: "Herrerasaurus",
+    image: "herrerasaurus.png",
+    period: "Triassic",
+    fullPeriod: "Late Triassic",
+    yearsMya: { start: 231, end: 225 },
+    pronunciation: "heh-RAY-rah-sore-us",
+    meaning: "'Herrera's lizard'",
+    diet: "carnivorous",
+    length: 3.0, // in meters
+    foundIn: ["Argentina"]
+  },
+  {
+    name: "Liliensternus",
+    image: "liliensternus.png",
+    period: "Triassic",
+    fullPeriod: "Late Triassic",
+    yearsMya: { start: 210, end: 205 },
+    pronunciation: "lil-ee-en-STER-nus",
+    meaning: "'von Lilienstern's lizard'",
+    diet: "carnivorous",
+    length: 5.2, // in meters
+    foundIn: ["Germany"]
+  },
+  {
+    name: "Lycorhinus",
+    image: "lycorhinus.png",
+    period: "Triassic",
+    fullPeriod: "Late Triassic",
+    yearsMya: { start: 201, end: 199 },
+    pronunciation: "lye-ko-RYE-nus",
+    meaning: "'wolf snout'",
+    diet: "herbivorous",
+    length: 1.2, // in meters
+    foundIn: ["South Africa"]
+  },
+  {
+    name: "Melanorosaurus",
+    image: "melanorosaurus.png",
+    period: "Triassic",
+    fullPeriod: "Late Triassic",
+    yearsMya: { start: 227, end: 221 },
+    pronunciation: "mel-an-oh-roe-sore-us",
+    meaning: "'Black Mountain lizard'",
+    diet: "herbivorous",
+    length: 8.0, // in meters
+    weight: 1300, // in kilograms
+    foundIn: ["South Africa"]
+  },
+  {
+    name: "Mussaurus",
+    image: "mussaurus.png",
+    period: "Triassic",
+    fullPeriod: "Late Triassic",
+    yearsMya: { start: 215, end: 203 },
+    pronunciation: "moo-sore-us",
+    meaning: "'mouse lizard'",
+    diet: "herbivorous",
+    length: 3.0, // in meters
+    foundIn: ["Argentina"]
+  },
+  {
+    name: "Plateosaurus",
+    image: "plateosaurus.png",
+    period: "Triassic",
+    fullPeriod: "Late Triassic",
+    yearsMya: { start: 214, end: 204 },
+    pronunciation: "plat-ee-oh-sore-us",
+    meaning: "'flat lizard'",
+    diet: "herbivorous",
+    length: 8.0, // in meters
+    foundIn: ["Germany", "Switzerland", "France", "Greenland"]
+  },
+  {
+    name: "Procompsognathus",
+    image: "procompsognathus.png",
+    period: "Triassic",
+    fullPeriod: "Late Triassic",
+    yearsMya: { start: 210, end: 205 },
+    pronunciation: "pro-comp-sog-NAY-thus",
+    meaning: "'before elegant jaw'",
+    diet: "carnivorous",
+    length: 1.2, // in meters
+    foundIn: ["Germany"]
+  },
+  {
+    name: "Riojasaurus",
+    image: "riojasaurus.png",
+    period: "Triassic",
+    fullPeriod: "Late Triassic",
+    yearsMya: { start: 227, end: 221 },
+    pronunciation: "ree-oh-ha-sore-us",
+    meaning: "'Rioja lizard'",
+    diet: "herbivorous",
+    length: 10.0, // in meters
+    foundIn: ["Argentina"]
+  },
+  {
+    name: "Staurikosaurus",
+    image: "staurikosaurus.png",
+    period: "Triassic",
+    fullPeriod: "Late Triassic",
+    yearsMya: { start: 227, end: 221 },
+    pronunciation: "stor-ik-oh-sore-us",
+    meaning: "'Southern Cross lizard'",
+    diet: "carnivorous",
+    length: 2.0, // in meters
+    foundIn: ["Brazil"]
+  },
+  {
+    name: "Thecodontosaurus",
+    image: "thecodontosaurus.png",
+    period: "Triassic",
+    fullPeriod: "Late Triassic",
+    yearsMya: { start: 227, end: 201 },
+    pronunciation: "thee-koh-don-toe-sore-us",
+    meaning: "'socket-tooth lizard'",
+    diet: "herbivorous",
+    length: 2.0, // in meters
+    foundIn: ["United Kingdom"]
   }
 ];
 
