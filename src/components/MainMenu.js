e73bd88d3a50546d1a65e67f9143dce712fe254a
Fraser Provan <fraserprovan2@gmail.com>
2025-03-03 02:23:15 +0000

    misc fixes

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -1,6 +1,4 @@
-
 import React from "react";
-import "../styles/main-menu.scss";
 
 function MainMenu({ startQuiz }) {
   return (
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import "../styles/quiz.scss";
 
 function Quiz({ questions, onQuit }) {
   const [currentIndex, setCurrentIndex] = useState(0);
